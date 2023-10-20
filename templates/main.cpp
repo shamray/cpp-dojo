f359d936e1157c96515f59aeaab1e53bbf0b8beb
Volodymyr Shamray <shamray@outlook.com>
2023-10-20 22:32:17 +0300

    Add .clang-format

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,24 +1,26 @@
-#include <variant>
 #include <print>
+#include <variant>
 
-template <typename... T>
-struct Visitor: T...
+namespace dojo
 {
+template <typename... T>
+struct Visitor: T... {
     using T::operator()...;
 };
 
 #if (__cplusplus <= 201703L)
-template <typename... T> Visitor(T...) -> Visitor<T...>;
+template <typename... T>
+Visitor(T...) -> Visitor<T...>;
 #endif
 
-int main() {
-    std::variant<int, float, std::string> intFloatString { "Hello" };
-
-    Visitor visitor{
-        [](const int& i) { std::println("int: {}", i); },
-        [](const float& f) { std::println("float: {}", f); },
-        [](const std::string& s) { std::println("string: {}", s); }
-    };
-    std::visit(visitor, intFloatString);
 }
 
+int main() {
+    std::variant<int, float, std::string> intFloatString{"Hello"};
+
+    dojo::Visitor visitor{
+        [](const int& i) { std::println("int: {}", i); },
+        [](const float& f) { std::println("float: {}", f); },
+        [](const std::string& s) { std::println("string: {}", s); }};
+    std::visit(visitor, intFloatString);
+}
